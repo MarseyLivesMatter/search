4d45f11b6ce73969ad2d47441d1628a6eaf3f92d
MarseyLivesMatter <91437068+MarseyLivesMatter@users.noreply.github.com>
2022-05-19 15:09:28 +0800

    Create mf.js

---
+++ src/mf.js
@@ -0,0 +1,7 @@
+  window._mfq = window._mfq || [];
+  (function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.defer = true;
+    mf.src = "//cdn.mouseflow.com/projects/1f7f6247-2022-46cc-a7fd-c7a90d4526f6.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+  })();
