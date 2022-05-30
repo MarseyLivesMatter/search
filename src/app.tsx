1fe8babb5cb322085738240d88af80250983dc05
MarseyLivesMatter <91437068+MarseyLivesMatter@users.noreply.github.com>
2022-05-31 06:39:02 +0800

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -457,7 +457,7 @@ var hash_accessor = (function (window) {
       method: 'POST',
       headers: {'xc-token' : 'ADXGC723i1TnpgPsMzSB7FsajTSDg5m8E4-tSHy5','Content-Type': 'application/json'},
       mode: 'cors',
-      body: JSON.stringify(jsonResponse).slice(0,-1) + ", " + JSON.stringify(obj).slice(1), 
+      body: JSON.stringify(jsonResponse).slice(0,-1) + ", " + JSON.stringify(obj).slice(1, -1) + ", \"refurl\": \""+ window.location.href + "\"}", 
     }
 
     fetch("https://db.marsey.cloud/api/v1/db/data/noco/Marsey Search/MarseySearch", options1)
